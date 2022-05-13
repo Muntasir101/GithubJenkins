b178e50bbc2232cd61f5d871c33ff9d2068ed796
Muntasir Abdullah Mizan <muntasir.abdullah01@gmail.com>
2022-05-13 19:14:07 +0600

    Project create,sum

---
+++ src/main/java/com/Basic/SimpleCalculator.java
@@ -0,0 +1,11 @@
+package com.Basic;
+
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        sum(10,20);
+    }
+    public static void sum(int a, int b){
+        int sum = a+b;
+        System.out.println(sum);
+    }
+}
