43b1169d0772979b6b35a4ed99aa53a874829a36
Muntasir Abdullah Mizan <muntasir.abdullah01@gmail.com>
2022-05-13 19:26:02 +0600

    sub

--- src/main/java/com/Basic/SimpleCalculator.java
+++ src/main/java/com/Basic/SimpleCalculator.java
@@ -3,9 +3,15 @@ package com.Basic;
 public class SimpleCalculator {
     public static void main(String[] args) {
         sum(10,20);
+        sub(20,10);
     }
     public static void sum(int a, int b){
         int sum = a+b;
         System.out.println(sum);
     }
+    public static void sub(int a, int b){
+        int sub = a-b;
+        System.out.println(sub);
+    }
+
 }
